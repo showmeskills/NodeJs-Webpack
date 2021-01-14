bf5f3198068dd755b82db8e2ce6433c67e1a359f
Terrance <465446853yan@gmail.com>
2021-01-14 22:24:17 +1100

    the basic construction of environmental webpack with vue

---
+++ Webpack03/src/app.js
@@ -0,0 +1,20 @@
+((x=1,y=2)=>{return x+y})();
+import '../assets/less/index.less';
+import '../assets/font/iconfont.css';
+import Vue from 'vue'
+new Vue({
+    el:'#app',
+    data:{
+        msg:'this is vue '
+    },
+})
+
+new Vue({
+    el:'#login',
+    data:{
+        msg:'this is login '
+    }
+})
+
+  
+   
---
+++ Webpack03/webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+module.exports={
+    entry: path.join(__dirname,'src','app.js'),
+    output:{
+        filename:'built.js',
+        path: path.join(__dirname,'build')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.less$/,
+                use:['style-loader','css-loader','less-loader']
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test:/\.(jpg|png|gif)$/,
+                loader:'url-loader',
+                options: {
+                    limit: 8 * 1024,
+                    name: '[hash:10].[ext]',
+                    esModule:false,
+                }
+            },
+            {
+                //处理html 中img 资源
+                test:/\.html$/,
+                loader:'html-loader',
+            },
+            {
+                test:/\.vue$/,
+                use:['vue-loader']
+            },
+            {
+                exclude:/\.(html|js|less|css|jpg|png|gif)/,
+                loader:'file-loader',
+                options:{
+                    name:'[hash:10].[ext]'
+                }
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({         
+                template:path.join(__dirname,'views','index.html')
+        }),
+        new VueLoaderPlugin()
+    ],
+    devServer:{
+        contentBase:path.join(__dirname,'build'),
+        compress:true,
+        open:true,
+        port:3000,
+    },
+    resolve:{
+        alias: {  // 修改vue被导入时候报的路径
+             'vue$':'vue/dist/vue.js'
+        },
+        extensions: ['.js', '.vue']
+    },
+    mode:'development'
+}
